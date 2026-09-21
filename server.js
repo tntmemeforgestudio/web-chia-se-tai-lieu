3b515ca7816c4eb125605582677079ab5fb20f0a
TNT MEMEFGE STUDIO <tntmemeforgestudio@gmail.com>
2026-09-21 21:44:24 +0700

    Update 0.2 server.js

--- server.js
+++ server.js
@@ -19,12 +19,8 @@ if (!MONGO_URI) {
         .catch(err => console.error('❌ Lỗi kết nối MongoDB:', err));
 }
 
-// 2. Cấu hình trực tiếp chính xác tuyệt đối theo đúng thông số trên Cloudinary của bro
-cloudinary.config({
-  cloud_name: 'ufpn14eq',
-  api_key: '524937465313367',
-  api_secret: 'JdNCLlYCR1cfpMH-TOugpjOQWRQ'
-});
+// 2. Tự động nhận diện cấu hình từ biến CLOUDINARY_URL trên Render
+cloudinary.config();
 
 // 3. Cấu hình Multer Storage tự động phân loại tệp tin tải lên Cloudinary
 const storage = new CloudinaryStorage({
